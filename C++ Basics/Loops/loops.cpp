335daed30852a3fac62242cbea3d990da2ab8aee
CoderNived <nivedshenoy0@gmail.com>
2026-01-24 21:51:20 +0530

    updating loops

---
+++ C++ Basics/Loops/loops.cpp
@@ -0,0 +1,37 @@
+// While loops
+#include<iostream>
+using namespace std;
+int main()
+{
+   int i;
+   while(i<=10){
+    cout<<i<<endl;
+    i++;
+   }
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n; 
+    i=1;
+    while(i<=n){
+        cout<<i<<endl;
+        i++;
+    }
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n; 
+    i=n;
+    while(i>=1){
+        cout<<i<<endl;
+        i--;
+    }
+}
