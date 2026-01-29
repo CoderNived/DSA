02b3406adfffbad442c5209f27c266b05cd8f312
CoderNived <nivedshenoy0@gmail.com>
2026-01-29 19:28:33 +0530

    updating folders

---
+++ Arrays/Basic/print_alternates.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespce std;
+
+vector<int> getAlternates(vector<int> &arr)
+{
+    vector<int> res;
+    for(int i =0; i<arr.size(); i++){
+        if(i%2 == 0){
+            res.push_back(arr[i]);
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    vector<int> arr = {10,20,30,40,50,60,70};
+    vector<int> alternates = getAlternates(arr);    
+    for(int x: alternates){
+        cout<<x<<" ";
+    }
+}
